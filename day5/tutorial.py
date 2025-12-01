983aad5691e4268afaefb2471f680dd0f9e493f5
iChicken <65528141+shpre1236@users.noreply.github.com>
2025-12-01 12:21:28 +0900

    Create tutorial.py

---
+++ day5/tutorial.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.init as init
+
+num_data = 10000
+num_epoch = 1000
+learning_rate = 0.01
+
+x = init.uniform_(torch.Tensor(num_data,1),0,6)
+y = (x - 3)**2 + 4
+noise = init.normal_(torch.FloatTensor(num_data,1), std=0.5)
+y_noise = y + noise
+
+model = nn.Sequential(
+    nn.Linear(1,10),
+    nn.ReLU(),
+    nn.Linear(10,30),
+    nn.ReLU(),
+    nn.Linear(30,10),
+    nn.ReLU(),
+    nn.Linear(10,1)
+)
+
+loss_func = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+loss_array = []
+for i in range(num_epoch):
+    #initialize
+    optimizer.zero_grad()
+
+    # calculate output and loss
+    output = model(x)
+    loss = loss_func(output, y_noise)
+
+    # calculate gradient
+    loss.backward()
+
+    # update
+    optimizer.step()
+
+    # (optional) save loss value
+    loss_array.append(loss.item())
+
+print(model(torch.FloatTensor([3.0])).item())
+print(model(torch.FloatTensor([4.0])).item())
+print(model(torch.FloatTensor([2.0])).item())
