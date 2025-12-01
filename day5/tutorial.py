a550e35a56d29a527f1a00cf717c6b8c3fec2dad
iChicken <65528141+shpre1236@users.noreply.github.com>
2025-12-01 12:57:55 +0900

    Update tutorial.py

--- day5/tutorial.py
+++ day5/tutorial.py
@@ -46,3 +46,18 @@ for i in range(num_epoch):
 print(model(torch.FloatTensor([3.0])).item())
 print(model(torch.FloatTensor([4.0])).item())
 print(model(torch.FloatTensor([2.0])).item())
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.arange(0, 6, 0.01)
+y_pred = np.array([])
+y_real = np.array([])
+for i in range(t.size):
+    y_pred = np.append(y_pred, model(torch.FloatTensor([t[i]])).item())
+    y_real = np.append(y_real, (t[i] - 3) ** 2 + 4)
+
+plt.figure()
+plt.plot(x, y_noise, '.k')
+plt.plot(t, y_real, 'b-')
+plt.plot(t, y_pred, 'r-')
